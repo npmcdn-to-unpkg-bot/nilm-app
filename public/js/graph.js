e8de6a3a91d87289b92235c3e65d4fa98cebbc65
zutshisunakshi <zutshisunakshi@gmail.com>
2016-06-19 19:26:29 -0500

    allah

--- public/js/graph.js
+++ public/js/graph.js
@@ -2,9 +2,11 @@ $(function () {
 
     $.getJSON('https://api.mlab.com/api/1/databases?apiKey=2jtlnQ_QQGBUsPvQNztbewJaHaRcEYha', function (data) {
         // Create the chart
-        $('#view').highcharts('StockChart', {
+       var chart1 = new Highcharts.StockChart({
 
-
+    chart:  {
+            renderTo: 'view'
+            },
             rangeSelector : {
                 selected : 1
             },
