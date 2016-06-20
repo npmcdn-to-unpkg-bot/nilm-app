1eff91be45ed5654da847338902c7f9a94629482
zutshisunakshi <zutshisunakshi@gmail.com>
2016-06-19 23:20:19 -0500

    al

--- public/js/graph.js
+++ public/js/graph.js
@@ -1,11 +1,22 @@
 $(function () {
+     
+     var mySeries = [];
+    
+         
+     
+        for (var i = 0; i < AllPower.length; i++) {
+            mySeries.push([AllPower.Timestamp, AllPower.power]);
+            i++
+        }
+
+
 
     $.getJSON('https://api.mlab.com/api/1/databases?apiKey=2jtlnQ_QQGBUsPvQNztbewJaHaRcEYha', function (data) {
         // Create the chart
-       chart1 = new Highcharts.Chart({
+      // chart1 = new Highcharts.Chart({
 
     chart:  {
-            renderTo: 'view',
+            //renderTo: 'view',
             type: 'line'
             },
             rangeSelector : {
@@ -18,12 +29,14 @@ $(function () {
 
             series : [{
                 name : 'Power',
-                data : data,
+                data : [],
                 tooltip: {
                     valueDecimals: 2
                 }
             }]
-        });
+            var chart1 = $('#view').highcharts();
+            chart1.series[0].setData(mySeries);
+      //  });
     });
 
 });
