891f70fa1c2c4e6e479d8355f6f8b87b35e2a91d
zutshisunakshi <zutshisunakshi@gmail.com>
2016-06-19 23:28:11 -0500

    al

--- public/js/graph.js
+++ public/js/graph.js
@@ -11,13 +11,13 @@ $(function () {
 
 
 
-    $.getJSON('https://api.mlab.com/api/1/databases?apiKey=2jtlnQ_QQGBUsPvQNztbewJaHaRcEYha', function (data) {
+    //$.getJSON('https://api.mlab.com/api/1/databases?apiKey=2jtlnQ_QQGBUsPvQNztbewJaHaRcEYha', function (data) {
         // Create the chart
       // chart1 = new Highcharts.Chart({
 
     chart:  {
-            renderTo: 'view',
-            type: 'line'
+           // renderTo: 'view',
+            //type: 'line'
             },
             rangeSelector : {
                 selected : 1
@@ -39,4 +39,4 @@ $(function () {
       //  });
     });
 
-});
+///});
